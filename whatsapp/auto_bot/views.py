1e93505cd3a80cf3c0edd80d40841dee4e2c9ea4
Paul <awolesiboluwatife@gmail.com>
2022-12-14 07:15:35 +0100

    fixing the post request issue

--- whatsapp/auto_bot/views.py
+++ whatsapp/auto_bot/views.py
@@ -8,45 +8,23 @@ from whatsapp.users.models import User
 @csrf_exempt
 def twilio(request):
 
-    # # user input
-    # user_msg = request.POST.get('Body').lower()
+    # user input
 
-    # # creating object of MessagingResponse
-    # response = MessagingResponse()
+    if request.method == "POST":
+        user_msg = request.POST.get('Body')
+        # creating object of MessagingResponse
+        response = MessagingResponse()
 
-    # # User Query
-    # name = user_msg.split()[0].lower()
-    # if User.object.filter(name=name).exists():
-    #     user = User.object.get(name=name)
-    #     user.activity = user_msg
-    #     user.save()
-
-    # # list to store urls
-    # response.message("Your response has been saved")
-    user_msg = request.values.get('Body', '').lower()
-    print("hope this works", flush=True)
-    print(request.values, flush=True)
-
-    # creating object of MessagingResponse
-    response = MessagingResponse()
-
-    # User Query
-    q = user_msg + "geeksforgeeks.org"
+        # User Query
+        name = user_msg.split()[0].lower()
+        name = user_msg
+        if User.objects.filter(name=name).exists():
+            user = User.objects.get(name=name)
+            user.activity = user_msg
+            user.save()
 
     # list to store urls
-    result = []
+    response.message("Your response has been saved")
 
-    # searching and storing urls
-    # for i in search(q, tld='co.in', num=6, stop=6, pause=2):
-    #     result.append(i)
-
-    # displaying result
-    msg = response.message(f"--- Result for '{user_msg}' are  ---")
-
-    # msg = response.message(result[0])
-    # msg = response.message(result[1])
-    # msg = response.message(result[2])
-    # msg = response.message(result[3])
-    # msg = response.message(result[4])
 
     return HttpResponse(str(response))
